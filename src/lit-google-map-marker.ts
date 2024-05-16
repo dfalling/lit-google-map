8cf2536c9e14ffdb47f6ea9b0516a7d167011101
dfalling <dfalling@gmail.com>
2024-05-16 14:57:43 +0200

    Switch whitespace to spaces (#109)

--- src/lit-google-map-marker.ts
+++ src/lit-google-map-marker.ts
@@ -3,236 +3,236 @@ import { customElement, property } from "lit/decorators.js";
 
 @customElement("lit-google-map-marker")
 export class LitGoogleMapMarker extends LitElement {
-	@property({ type: Number, reflect: true })
-	latitude = 0;
+  @property({ type: Number, reflect: true })
+  latitude = 0;
 
-	@property({ type: Number, reflect: true })
-	longitude = 0;
+  @property({ type: Number, reflect: true })
+  longitude = 0;
 
-	@property({ type: Number, reflect: true, attribute: "z-index" })
-	zIndex = 0;
+  @property({ type: Number, reflect: true, attribute: "z-index" })
+  zIndex = 0;
 
-	@property({ type: Boolean, reflect: true })
-	open = false;
+  @property({ type: Boolean, reflect: true })
+  open = false;
 
-	@property({ type: String, reflect: true })
-	id: string | null = null;
+  @property({ type: String, reflect: true })
+  id: string | null = null;
 
-	@property({ type: String, reflect: true })
-	glyph: string | null = null;
+  @property({ type: String, reflect: true })
+  glyph: string | null = null;
 
-	@property({ type: String, reflect: true })
-	glyphColor: string | null = null;
+  @property({ type: String, reflect: true })
+  glyphColor: string | null = null;
 
-	@property({ type: String, reflect: true })
-	background: string | null = null;
+  @property({ type: String, reflect: true })
+  background: string | null = null;
 
-	@property({ type: String, reflect: true })
-	borderColor: string | null = null;
+  @property({ type: String, reflect: true })
+  borderColor: string | null = null;
 
-	@property({ type: Number, reflect: true })
-	scale: number = null;
+  @property({ type: Number, reflect: true })
+  scale: number = null;
 
-	/**
-	 * If set, the marker will not be used to calculate the map's bounds.
-	 */
-	@property({ type: Boolean, attribute: "omit-from-fit" })
-	omitFromFit = false;
+  /**
+   * If set, the marker will not be used to calculate the map's bounds.
+   */
+  @property({ type: Boolean, attribute: "omit-from-fit" })
+  omitFromFit = false;
 
-	map: google.maps.Map = null;
-	marker: google.maps.marker.AdvancedMarkerElement = null;
-	pin: google.maps.marker.PinElement = null;
-	info: google.maps.InfoWindow;
-	contentObserver: MutationObserver;
-	openInfoHandler: google.maps.MapsEventListener;
-	closeInfoHandler: google.maps.MapsEventListener;
+  map: google.maps.Map = null;
+  marker: google.maps.marker.AdvancedMarkerElement = null;
+  pin: google.maps.marker.PinElement = null;
+  info: google.maps.InfoWindow;
+  contentObserver: MutationObserver;
+  openInfoHandler: google.maps.MapsEventListener;
+  closeInfoHandler: google.maps.MapsEventListener;
 
-	attributeChangedCallback(name: string, oldval: string, newval: string) {
-		super.attributeChangedCallback(name, oldval, newval);
-		switch (name) {
-			case "open": {
-				this.openChanged();
-				break;
-			}
-			case "latitude": {
-				this.updatePosition();
-				break;
-			}
-			case "longitude": {
-				this.updatePosition();
-				break;
-			}
-			case "glyph": {
-				if (this.pin) {
-					this.pin.glyph = this.glyph;
-				}
-				break;
-			}
-			case "glyphColor": {
-				if (this.pin) {
-					this.pin.glyphColor = this.glyphColor;
-				}
-				break;
-			}
-			case "background": {
-				if (this.pin) {
-					this.pin.background = this.background;
-				}
-				break;
-			}
-			case "borderColor": {
-				if (this.pin) {
-					this.pin.borderColor = this.borderColor;
-				}
-				break;
-			}
-			case "scale": {
-				if (this.pin) {
-					this.pin.scale = this.scale;
-				}
-				break;
-			}
-			case "z-index": {
-				if (this.marker) {
-					this.marker.zIndex = this.zIndex;
-				}
-				break;
-			}
-		}
-	}
+  attributeChangedCallback(name: string, oldval: string, newval: string) {
+    super.attributeChangedCallback(name, oldval, newval);
+    switch (name) {
+      case "open": {
+        this.openChanged();
+        break;
+      }
+      case "latitude": {
+        this.updatePosition();
+        break;
+      }
+      case "longitude": {
+        this.updatePosition();
+        break;
+      }
+      case "glyph": {
+        if (this.pin) {
+          this.pin.glyph = this.glyph;
+        }
+        break;
+      }
+      case "glyphColor": {
+        if (this.pin) {
+          this.pin.glyphColor = this.glyphColor;
+        }
+        break;
+      }
+      case "background": {
+        if (this.pin) {
+          this.pin.background = this.background;
+        }
+        break;
+      }
+      case "borderColor": {
+        if (this.pin) {
+          this.pin.borderColor = this.borderColor;
+        }
+        break;
+      }
+      case "scale": {
+        if (this.pin) {
+          this.pin.scale = this.scale;
+        }
+        break;
+      }
+      case "z-index": {
+        if (this.marker) {
+          this.marker.zIndex = this.zIndex;
+        }
+        break;
+      }
+    }
+  }
 
-	openChanged() {
-		if (!this.info) return;
+  openChanged() {
+    if (!this.info) return;
 
-		if (this.open) {
-			this.info.open(this.map, this.marker);
-			this.dispatchEvent(
-				new CustomEvent("google-map-marker-open", { bubbles: true }),
-			);
-		} else {
-			this.info.close();
-			this.dispatchEvent(
-				new CustomEvent("google-map-marker-close", { bubbles: true }),
-			);
-		}
-	}
+    if (this.open) {
+      this.info.open(this.map, this.marker);
+      this.dispatchEvent(
+        new CustomEvent("google-map-marker-open", { bubbles: true }),
+      );
+    } else {
+      this.info.close();
+      this.dispatchEvent(
+        new CustomEvent("google-map-marker-close", { bubbles: true }),
+      );
+    }
+  }
 
-	updatePosition() {
-		if (this.marker) {
-			this.marker.position = new google.maps.LatLng(
-				this.latitude,
-				this.longitude,
-			);
-		}
-	}
+  updatePosition() {
+    if (this.marker) {
+      this.marker.position = new google.maps.LatLng(
+        this.latitude,
+        this.longitude,
+      );
+    }
+  }
 
-	changeMap(newMap: google.maps.Map) {
-		this.map = newMap;
-		this.mapChanged();
-	}
+  changeMap(newMap: google.maps.Map) {
+    this.map = newMap;
+    this.mapChanged();
+  }
 
-	mapChanged() {
-		// Marker will be rebuilt, so disconnect existing one from old map and listeners.
-		if (this.marker) {
-			this.marker.map = null;
-			google.maps.event.clearInstanceListeners(this.marker);
-		}
+  mapChanged() {
+    // Marker will be rebuilt, so disconnect existing one from old map and listeners.
+    if (this.marker) {
+      this.marker.map = null;
+      google.maps.event.clearInstanceListeners(this.marker);
+    }
 
-		if (this.map && this.map instanceof google.maps.Map) {
-			this.mapReady();
-		}
-	}
+    if (this.map && this.map instanceof google.maps.Map) {
+      this.mapReady();
+    }
+  }
 
-	mapReady() {
-		this.pin = new google.maps.marker.PinElement({
-			glyph: this.glyph,
-			glyphColor: this.glyphColor,
-			background: this.background,
-			borderColor: this.borderColor,
-			scale: this.scale,
-		});
-		this.marker = new google.maps.marker.AdvancedMarkerElement({
-			map: this.map,
-			position: {
-				lat: this.latitude,
-				lng: this.longitude,
-			},
-			content: this.pin.element,
-			zIndex: this.zIndex,
-			gmpClickable: true,
-		});
+  mapReady() {
+    this.pin = new google.maps.marker.PinElement({
+      glyph: this.glyph,
+      glyphColor: this.glyphColor,
+      background: this.background,
+      borderColor: this.borderColor,
+      scale: this.scale,
+    });
+    this.marker = new google.maps.marker.AdvancedMarkerElement({
+      map: this.map,
+      position: {
+        lat: this.latitude,
+        lng: this.longitude,
+      },
+      content: this.pin.element,
+      zIndex: this.zIndex,
+      gmpClickable: true,
+    });
 
-		this.pin.element.addEventListener("mouseover", () => {
-			this.dispatchEvent(
-				new CustomEvent("mouseover", {
-					detail: { id: this.id },
-					bubbles: true,
-					composed: true,
-				}),
-			);
-		});
+    this.pin.element.addEventListener("mouseover", () => {
+      this.dispatchEvent(
+        new CustomEvent("mouseover", {
+          detail: { id: this.id },
+          bubbles: true,
+          composed: true,
+        }),
+      );
+    });
 
-		this.pin.element.addEventListener("mouseout", () => {
-			this.dispatchEvent(
-				new CustomEvent("mouseout", {
-					detail: { id: this.id },
-					bubbles: true,
-					composed: true,
-				}),
-			);
-		});
+    this.pin.element.addEventListener("mouseout", () => {
+      this.dispatchEvent(
+        new CustomEvent("mouseout", {
+          detail: { id: this.id },
+          bubbles: true,
+          composed: true,
+        }),
+      );
+    });
 
-		this.marker.addListener("click", () => {
-			this.dispatchEvent(
-				new CustomEvent("click", {
-					detail: { id: this.id },
-					bubbles: true,
-					composed: true,
-				}),
-			);
-		});
+    this.marker.addListener("click", () => {
+      this.dispatchEvent(
+        new CustomEvent("click", {
+          detail: { id: this.id },
+          bubbles: true,
+          composed: true,
+        }),
+      );
+    });
 
-		this.contentChanged();
-	}
+    this.contentChanged();
+  }
 
-	contentChanged() {
-		if (this.contentObserver) this.contentObserver.disconnect();
+  contentChanged() {
+    if (this.contentObserver) this.contentObserver.disconnect();
 
-		this.contentObserver = new MutationObserver(this.contentChanged.bind(this));
-		this.contentObserver.observe(this, {
-			childList: true,
-			subtree: true,
-		});
+    this.contentObserver = new MutationObserver(this.contentChanged.bind(this));
+    this.contentObserver.observe(this, {
+      childList: true,
+      subtree: true,
+    });
 
-		const content = this.innerHTML.trim();
-		if (content) {
-			if (!this.info) {
-				this.info = new google.maps.InfoWindow();
+    const content = this.innerHTML.trim();
+    if (content) {
+      if (!this.info) {
+        this.info = new google.maps.InfoWindow();
 
-				this.openInfoHandler = google.maps.event.addListener(
-					this.marker,
-					"click",
-					function () {
-						this.open = true;
-					}.bind(this),
-				);
+        this.openInfoHandler = google.maps.event.addListener(
+          this.marker,
+          "click",
+          function () {
+            this.open = true;
+          }.bind(this),
+        );
 
-				this.closeInfoHandler = google.maps.event.addListener(
-					this.info,
-					"closeclick",
-					function () {
-						this.open = false;
-					}.bind(this),
-				);
-			}
-			this.info.setContent(content);
-		} else {
-			if (this.info) {
-				// Destroy the existing infowindow.  It doesn't make sense to have an empty one.
-				google.maps.event.removeListener(this.openInfoHandler);
-				google.maps.event.removeListener(this.closeInfoHandler);
-				this.info = null;
-			}
-		}
-	}
+        this.closeInfoHandler = google.maps.event.addListener(
+          this.info,
+          "closeclick",
+          function () {
+            this.open = false;
+          }.bind(this),
+        );
+      }
+      this.info.setContent(content);
+    } else {
+      if (this.info) {
+        // Destroy the existing infowindow.  It doesn't make sense to have an empty one.
+        google.maps.event.removeListener(this.openInfoHandler);
+        google.maps.event.removeListener(this.closeInfoHandler);
+        this.info = null;
+      }
+    }
+  }
 }
--- src/lit-google-map-polygon.ts
+++ src/lit-google-map-polygon.ts
@@ -4,53 +4,53 @@ import type { Shape } from "./shape";
 
 @customElement("lit-google-map-polygon")
 export class LitGoogleMapPolygon extends LitElement implements Shape {
-	@property({ type: Array })
-	paths: object[] = [];
+  @property({ type: Array })
+  paths: object[] = [];
 
-	@property({ type: String, attribute: "fill-color" })
-	fillColor = "#FF0000";
+  @property({ type: String, attribute: "fill-color" })
+  fillColor = "#FF0000";
 
-	@property({ type: Number, attribute: "fill-opacity" })
-	fillOpacity = 0.35;
+  @property({ type: Number, attribute: "fill-opacity" })
+  fillOpacity = 0.35;
 
-	@property({ type: String, attribute: "stroke-color" })
-	strokeColor = "#FF0000";
+  @property({ type: String, attribute: "stroke-color" })
+  strokeColor = "#FF0000";
 
-	@property({ type: Number, attribute: "stroke-opacity" })
-	strokeOpacity = 0.8;
+  @property({ type: Number, attribute: "stroke-opacity" })
+  strokeOpacity = 0.8;
 
-	@property({ type: Number, attribute: "stroke-weight" })
-	strokeWeight = 2;
+  @property({ type: Number, attribute: "stroke-weight" })
+  strokeWeight = 2;
 
-	map: google.maps.Map = null;
-	polygon: google.maps.Polygon = null;
+  map: google.maps.Map = null;
+  polygon: google.maps.Polygon = null;
 
-	attachToMap(map: google.maps.Map): void {
-		this.map = map;
-		this.mapChanged();
-	}
+  attachToMap(map: google.maps.Map): void {
+    this.map = map;
+    this.mapChanged();
+  }
 
-	mapChanged() {
-		// Polygon will be rebuilt, so disconnect existing one from old map and listeners.
-		if (this.polygon) {
-			this.polygon.setMap(null);
-			google.maps.event.clearInstanceListeners(this.polygon);
-		}
+  mapChanged() {
+    // Polygon will be rebuilt, so disconnect existing one from old map and listeners.
+    if (this.polygon) {
+      this.polygon.setMap(null);
+      google.maps.event.clearInstanceListeners(this.polygon);
+    }
 
-		if (this.map && this.map instanceof google.maps.Map) {
-			this.mapReady();
-		}
-	}
+    if (this.map && this.map instanceof google.maps.Map) {
+      this.mapReady();
+    }
+  }
 
-	mapReady() {
-		this.polygon = new google.maps.Polygon({
-			map: this.map,
-			strokeColor: this.strokeColor,
-			strokeOpacity: this.strokeOpacity,
-			strokeWeight: this.strokeWeight,
-			fillColor: this.fillColor,
-			fillOpacity: this.fillOpacity,
-			paths: this.paths,
-		});
-	}
+  mapReady() {
+    this.polygon = new google.maps.Polygon({
+      map: this.map,
+      strokeColor: this.strokeColor,
+      strokeOpacity: this.strokeOpacity,
+      strokeWeight: this.strokeWeight,
+      fillColor: this.fillColor,
+      fillOpacity: this.fillOpacity,
+      paths: this.paths,
+    });
+  }
 }
