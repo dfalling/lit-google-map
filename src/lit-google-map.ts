dbbc4b22aa86ca8898d5089fdb2aa72830949c81
Dennis Fallin <dfalling@gmail.com>
2024-05-16 12:19:21 +0200

    Add biome and apply automatic fixes

--- src/lit-google-map.ts
+++ src/lit-google-map.ts
@@ -1,295 +1,295 @@
-import { LitElement, html, css } from "lit";
+import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import { LitGoogleMapsApi } from "./lit-google-maps-api";
-import { LitGoogleMapMarker } from "./lit-google-map-marker";
-import { LitSelector } from "./lit-selector";
-import { Shape } from "./shape";
+import type { LitGoogleMapMarker } from "./lit-google-map-marker";
+import type { LitGoogleMapsApi } from "./lit-google-maps-api";
+import type { LitSelector } from "./lit-selector";
+import type { Shape } from "./shape";
 
 @customElement("lit-google-map")
 export class LitGoogleMap extends LitElement {
-  /**
-   * A Maps API key. To obtain an API key, see https://developers.google.com/maps/documentation/javascript/tutorial#api_key.
-   */
-  @property({ type: String, attribute: "api-key" })
-  apiKey: string = "";
+	/**
+	 * A Maps API key. To obtain an API key, see https://developers.google.com/maps/documentation/javascript/tutorial#api_key.
+	 */
+	@property({ type: String, attribute: "api-key" })
+	apiKey = "";
 
-  /**
-   * Version of the Google Maps API to use.
-   */
-  @property({ type: String })
-  version: string = "3.48";
+	/**
+	 * Version of the Google Maps API to use.
+	 */
+	@property({ type: String })
+	version = "3.48";
 
-  /**
-   * If set, custom styles can be applied to the map.
-   * For style documentation see https://developers.google.com/maps/documentation/javascript/reference#MapTypeStyle
-   */
-  @property({ type: Object })
-  styles: object = {};
+	/**
+	 * If set, custom styles can be applied to the map.
+	 * For style documentation see https://developers.google.com/maps/documentation/javascript/reference#MapTypeStyle
+	 */
+	@property({ type: Object })
+	styles: object = {};
 
-  /**
-   * A zoom level to set the map to.
-   */
-  @property({ type: Number })
-  zoom: number = 8;
+	/**
+	 * A zoom level to set the map to.
+	 */
+	@property({ type: Number })
+	zoom = 8;
 
-  /**
-   * If set, the zoom level is set such that all markers (google-map-marker children) are brought into view.
-   */
-  @property({ type: Boolean, attribute: "fit-to-markers" })
-  fitToMarkers: boolean = false;
+	/**
+	 * If set, the zoom level is set such that all markers (google-map-marker children) are brought into view.
+	 */
+	@property({ type: Boolean, attribute: "fit-to-markers" })
+	fitToMarkers = false;
 
-  /**
-   * Map type to display. One of 'roadmap', 'satellite', 'hybrid', 'terrain'.
-   */
-  @property({ type: String, attribute: "map-type" })
-  mapType: string = "roadmap";
+	/**
+	 * Map type to display. One of 'roadmap', 'satellite', 'hybrid', 'terrain'.
+	 */
+	@property({ type: String, attribute: "map-type" })
+	mapType = "roadmap";
 
-  @property({ type: Number, attribute: "center-latitude" })
-  centerLatitude: number = -34.397;
+	@property({ type: Number, attribute: "center-latitude" })
+	centerLatitude = -34.397;
 
-  @property({ type: Number, attribute: "center-longitude" })
-  centerLongitude: number = 150.644;
+	@property({ type: Number, attribute: "center-longitude" })
+	centerLongitude = 150.644;
 
-  @property({ type: String })
-  language: string = "";
+	@property({ type: String })
+	language = "";
 
-  // mapId is required for use of AdvancedMarkerElement
-  @property({ type: String, attribute: "map-id" })
-  mapId: string = "DEMO_MAP_ID";
+	// mapId is required for use of AdvancedMarkerElement
+	@property({ type: String, attribute: "map-id" })
+	mapId = "DEMO_MAP_ID";
 
-  map: google.maps.Map = null;
+	map: google.maps.Map = null;
 
-  markers: Array<Node>;
-  shapes: Array<Node>;
+	markers: Array<Node>;
+	shapes: Array<Node>;
 
-  markerObserverSet: boolean;
+	markerObserverSet: boolean;
 
-  initGMap() {
-    if (this.map != null) {
-      return; // already initialized
-    }
+	initGMap() {
+		if (this.map != null) {
+			return; // already initialized
+		}
 
-    var gMapApiElement = this.shadowRoot.getElementById(
-      "api",
-    ) as LitGoogleMapsApi;
+		var gMapApiElement = this.shadowRoot.getElementById(
+			"api",
+		) as LitGoogleMapsApi;
 
-    if (gMapApiElement == null || gMapApiElement.libraryLoaded != true) {
-      return;
-    }
+		if (gMapApiElement == null || gMapApiElement.libraryLoaded != true) {
+			return;
+		}
 
-    this.map = new google.maps.Map(
-      this.shadowRoot.getElementById("map"),
-      this.getMapOptions(),
-    );
+		this.map = new google.maps.Map(
+			this.shadowRoot.getElementById("map"),
+			this.getMapOptions(),
+		);
 
-    // bounds and tiles events ported from https://github.com/launchscout/lit-google-map/
-    this.map.addListener("bounds_changed", () => {
-      this.dispatchEvent(
-        new CustomEvent("bounds_changed", {
-          detail: this.map.getBounds().toJSON(),
-          bubbles: true,
-          composed: true,
-        }),
-      );
-    });
+		// bounds and tiles events ported from https://github.com/launchscout/lit-google-map/
+		this.map.addListener("bounds_changed", () => {
+			this.dispatchEvent(
+				new CustomEvent("bounds_changed", {
+					detail: this.map.getBounds().toJSON(),
+					bubbles: true,
+					composed: true,
+				}),
+			);
+		});
 
-    this.map.addListener("tilesloaded", () => {
-      this.dispatchEvent(
-        new CustomEvent("tilesloaded", {
-          detail: this.map.getBounds().toJSON(),
-          bubbles: true,
-          composed: true,
-        }),
-      );
-    });
+		this.map.addListener("tilesloaded", () => {
+			this.dispatchEvent(
+				new CustomEvent("tilesloaded", {
+					detail: this.map.getBounds().toJSON(),
+					bubbles: true,
+					composed: true,
+				}),
+			);
+		});
 
-    // https://developers.google.com/maps/documentation/javascript/examples/event-poi
-    this.map.addListener(
-      "click",
-      (event: google.maps.MapMouseEvent | google.maps.IconMouseEvent) => {
-        // event is IconMouseEvent
-        if ("placeId" in event) {
-          // prevent click from showing default google info window
-          event.stop();
-          this.dispatchEvent(
-            new CustomEvent("place_click", {
-              detail: { placeId: event.placeId },
-              bubbles: true,
-              composed: true,
-            }),
-          );
-        }
-      },
-    );
+		// https://developers.google.com/maps/documentation/javascript/examples/event-poi
+		this.map.addListener(
+			"click",
+			(event: google.maps.MapMouseEvent | google.maps.IconMouseEvent) => {
+				// event is IconMouseEvent
+				if ("placeId" in event) {
+					// prevent click from showing default google info window
+					event.stop();
+					this.dispatchEvent(
+						new CustomEvent("place_click", {
+							detail: { placeId: event.placeId },
+							bubbles: true,
+							composed: true,
+						}),
+					);
+				}
+			},
+		);
 
-    this.updateMarkers();
-    this.updateShapes();
-  }
+		this.updateMarkers();
+		this.updateShapes();
+	}
 
-  getMapOptions(): google.maps.MapOptions {
-    return {
-      zoom: this.zoom,
-      center: { lat: this.centerLatitude, lng: this.centerLongitude },
-      mapTypeId: this.mapType,
-      // @ts-ignore
-      styles: this.styles,
-      mapId: this.mapId,
-    };
-  }
+	getMapOptions(): google.maps.MapOptions {
+		return {
+			zoom: this.zoom,
+			center: { lat: this.centerLatitude, lng: this.centerLongitude },
+			mapTypeId: this.mapType,
+			// @ts-ignore
+			styles: this.styles,
+			mapId: this.mapId,
+		};
+	}
 
-  mapApiLoaded() {
-    this.initGMap();
-  }
+	mapApiLoaded() {
+		this.initGMap();
+	}
 
-  connectedCallback() {
-    super.connectedCallback();
+	connectedCallback() {
+		super.connectedCallback();
 
-    this.initGMap();
-  }
+		this.initGMap();
+	}
 
-  attachChildrenToMap(children: Array<Node>) {
-    if (this.map) {
-      for (let child of children) {
-        (child as LitGoogleMapMarker).changeMap(this.map);
-      }
-    }
-  }
+	attachChildrenToMap(children: Array<Node>) {
+		if (this.map) {
+			for (const child of children) {
+				(child as LitGoogleMapMarker).changeMap(this.map);
+			}
+		}
+	}
 
-  detachChildrenFromMap(children: Array<Node>) {
-    if (this.map) {
-      for (let child of children) {
-        (child as LitGoogleMapMarker).changeMap(null);
-      }
-    }
-  }
+	detachChildrenFromMap(children: Array<Node>) {
+		if (this.map) {
+			for (const child of children) {
+				(child as LitGoogleMapMarker).changeMap(null);
+			}
+		}
+	}
 
-  observeMarkers() {
-    if (this.markerObserverSet) return;
+	observeMarkers() {
+		if (this.markerObserverSet) return;
 
-    this.addEventListener("selector-items-changed", () => {
-      this.updateMarkers();
-    });
-    this.markerObserverSet = true;
-  }
+		this.addEventListener("selector-items-changed", () => {
+			this.updateMarkers();
+		});
+		this.markerObserverSet = true;
+	}
 
-  updateMarkers() {
-    this.observeMarkers();
+	updateMarkers() {
+		this.observeMarkers();
 
-    const markersSelector = this.shadowRoot.getElementById(
-      "markers-selector",
-    ) as LitSelector;
-    if (!markersSelector) return;
+		const markersSelector = this.shadowRoot.getElementById(
+			"markers-selector",
+		) as LitSelector;
+		if (!markersSelector) return;
 
-    const newMarkers = markersSelector.items;
+		const newMarkers = markersSelector.items;
 
-    // do not recompute if markers have not been added or removed
-    if (this.markers && newMarkers.length === this.markers.length) {
-      const added = newMarkers.filter((m) => {
-        return this.markers && this.markers.indexOf(m) === -1;
-      });
-      if (added.length == 0) return;
-    }
+		// do not recompute if markers have not been added or removed
+		if (this.markers && newMarkers.length === this.markers.length) {
+			const added = newMarkers.filter((m) => {
+				return this.markers && this.markers.indexOf(m) === -1;
+			});
+			if (added.length == 0) return;
+		}
 
-    const boundsChanged = this.checkBoundsChanged(this.markers, newMarkers);
+		const boundsChanged = this.checkBoundsChanged(this.markers, newMarkers);
 
-    const removedMarkers =
-      this.markers?.filter((m) => {
-        return newMarkers.indexOf(m) === -1;
-      }) || [];
+		const removedMarkers =
+			this.markers?.filter((m) => {
+				return newMarkers.indexOf(m) === -1;
+			}) || [];
 
-    this.detachChildrenFromMap(removedMarkers);
+		this.detachChildrenFromMap(removedMarkers);
 
-    this.markers = newMarkers;
+		this.markers = newMarkers;
 
-    this.attachChildrenToMap(this.markers);
+		this.attachChildrenToMap(this.markers);
 
-    if (this.fitToMarkers && boundsChanged) {
-      this.fitToMarkersChanged();
-    }
-  }
+		if (this.fitToMarkers && boundsChanged) {
+			this.fitToMarkersChanged();
+		}
+	}
 
-  updateShapes() {
-    var shapesSelector = this.shadowRoot.getElementById(
-      "shapes-selector",
-    ) as LitSelector;
-    if (!shapesSelector) return;
+	updateShapes() {
+		var shapesSelector = this.shadowRoot.getElementById(
+			"shapes-selector",
+		) as LitSelector;
+		if (!shapesSelector) return;
 
-    this.shapes = shapesSelector.items;
+		this.shapes = shapesSelector.items;
 
-    for (let s of this.shapes) {
-      (s as unknown as Shape).attachToMap(this.map);
-    }
-  }
+		for (const s of this.shapes) {
+			(s as unknown as Shape).attachToMap(this.map);
+		}
+	}
 
-  fitToMarkersChanged(retryAttempt = 0) {
-    const markers = this.markers.filter(
-      (m) => !(m as LitGoogleMapMarker).omitFromFit,
-    );
-    if (this.map && this.fitToMarkers && markers.length > 0) {
-      const latLngBounds = new google.maps.LatLngBounds();
-      for (var marker of markers) {
-        latLngBounds.extend(
-          new google.maps.LatLng(
-            (marker as LitGoogleMapMarker).latitude,
-            (marker as LitGoogleMapMarker).longitude,
-          ),
-        );
-      }
+	fitToMarkersChanged(retryAttempt = 0) {
+		const markers = this.markers.filter(
+			(m) => !(m as LitGoogleMapMarker).omitFromFit,
+		);
+		if (this.map && this.fitToMarkers && markers.length > 0) {
+			const latLngBounds = new google.maps.LatLngBounds();
+			for (var marker of markers) {
+				latLngBounds.extend(
+					new google.maps.LatLng(
+						(marker as LitGoogleMapMarker).latitude,
+						(marker as LitGoogleMapMarker).longitude,
+					),
+				);
+			}
 
-      // fitBounds while the map has no width or height will zoom out to the whole world.
-      // We have to wait for valid dimensions before fitting bounds.
-      const domDimensions = this.getBoundingClientRect();
-      if (domDimensions.width === 0 || domDimensions.height === 0) {
-        console.log("Invalid DOM width or height for lit-google-map");
-        const timeout = 2 ** retryAttempt * 100;
-        setTimeout(() => {
-          this.fitToMarkersChanged(retryAttempt + 1);
-        }, timeout);
-        return;
-      }
+			// fitBounds while the map has no width or height will zoom out to the whole world.
+			// We have to wait for valid dimensions before fitting bounds.
+			const domDimensions = this.getBoundingClientRect();
+			if (domDimensions.width === 0 || domDimensions.height === 0) {
+				console.log("Invalid DOM width or height for lit-google-map");
+				const timeout = 2 ** retryAttempt * 100;
+				setTimeout(() => {
+					this.fitToMarkersChanged(retryAttempt + 1);
+				}, timeout);
+				return;
+			}
 
-      // For one marker, don't alter zoom, just center it.
-      if (markers.length > 1) {
-        this.map.fitBounds(latLngBounds, 0);
-      }
-      this.map.setCenter(latLngBounds.getCenter());
-    }
-  }
+			// For one marker, don't alter zoom, just center it.
+			if (markers.length > 1) {
+				this.map.fitBounds(latLngBounds, 0);
+			}
+			this.map.setCenter(latLngBounds.getCenter());
+		}
+	}
 
-  checkBoundsChanged(oldMarkers: Array<Node>, newMarkers: Array<Node>) {
-    const addedInBounds = newMarkers.filter((m) => {
-      return (
-        // skip items that are omitted from fit
-        !(m as LitGoogleMapMarker).omitFromFit &&
-        // if we have no markers, or the item wasn't there before, the bounds need to be updated
-        (!oldMarkers || !oldMarkers.includes(m))
-      );
-    });
-    const removedInBounds = oldMarkers?.filter((m) => {
-      return (
-        // skip items that are omitted from fit
-        !(m as LitGoogleMapMarker).omitFromFit &&
-        // if we have no markers, or the item isn't there anymore, the bounds need to be updated
-        (!newMarkers || !newMarkers.includes(m))
-      );
-    });
+	checkBoundsChanged(oldMarkers: Array<Node>, newMarkers: Array<Node>) {
+		const addedInBounds = newMarkers.filter((m) => {
+			return (
+				// skip items that are omitted from fit
+				!(m as LitGoogleMapMarker).omitFromFit &&
+				// if we have no markers, or the item wasn't there before, the bounds need to be updated
+				(!oldMarkers || !oldMarkers.includes(m))
+			);
+		});
+		const removedInBounds = oldMarkers?.filter((m) => {
+			return (
+				// skip items that are omitted from fit
+				!(m as LitGoogleMapMarker).omitFromFit &&
+				// if we have no markers, or the item isn't there anymore, the bounds need to be updated
+				(!newMarkers || !newMarkers.includes(m))
+			);
+		});
 
-    return addedInBounds.length > 0 || removedInBounds.length > 0;
-  }
+		return addedInBounds.length > 0 || removedInBounds.length > 0;
+	}
 
-  deselectMarker(event: Event) {}
+	deselectMarker(event: Event) {}
 
-  deselectShape(event: Event) {}
+	deselectShape(event: Event) {}
 
-  static styles = css`
+	static styles = css`
     #map {
       width: 100%;
       height: 100%;
     }
   `;
 
-  render() {
-    return html`
+	render() {
+		return html`
       <lit-google-maps-api
         id="api"
         api-key="${this.apiKey}"
@@ -317,5 +317,5 @@ export class LitGoogleMap extends LitElement {
       </lit-selector>
       <div id="map"></div>
     `;
-  }
+	}
 }
--- src/lit-google-maps-api.ts
+++ src/lit-google-maps-api.ts
@@ -3,130 +3,130 @@ import { customElement, property } from "lit/decorators.js";
 import { ScriptLoaderMap } from "./script-loader-map";
 
 abstract class JsonpLibraryElement extends LitElement {
-  public libraryLoaded: boolean = false;
+	public libraryLoaded = false;
 
-  public libraryErrorMessage: string | null = null;
+	public libraryErrorMessage: string | null = null;
 
-  abstract get libraryUrl(): string;
+	abstract get libraryUrl(): string;
 
-  abstract get notifyEvent(): string;
+	abstract get notifyEvent(): string;
 
-  get callbackName(): string | null {
-    return null;
-  }
+	get callbackName(): string | null {
+		return null;
+	}
 
-  private isReady: boolean = false;
+	private isReady = false;
 
-  libraryUrlChanged() {
-    // can't load before ready because notifyEvent might not be set
-    if (this.isReady && this.libraryUrl != null) this.loadLibrary();
-  }
+	libraryUrlChanged() {
+		// can't load before ready because notifyEvent might not be set
+		if (this.isReady && this.libraryUrl != null) this.loadLibrary();
+	}
 
-  libraryLoadCallback(error: Error, detail: any) {
-    if (error) {
-      console.warn("Library load failed:", error.message);
-      this.libraryErrorMessage = error.message;
-    } else {
-      this.libraryErrorMessage = null;
-      this.libraryLoaded = true;
-      if (this.notifyEvent != null) {
-        this.dispatchEvent(
-          new CustomEvent(this.notifyEvent, { detail: detail, composed: true }),
-        );
-      }
-    }
-  }
+	libraryLoadCallback(error: Error, detail: any) {
+		if (error) {
+			console.warn("Library load failed:", error.message);
+			this.libraryErrorMessage = error.message;
+		} else {
+			this.libraryErrorMessage = null;
+			this.libraryLoaded = true;
+			if (this.notifyEvent != null) {
+				this.dispatchEvent(
+					new CustomEvent(this.notifyEvent, { detail: detail, composed: true }),
+				);
+			}
+		}
+	}
 
-  /** loads the library, and fires this.notifyEvent upon completion */
-  loadLibrary() {
-    ScriptLoaderMap.getInstance().require(
-      this.libraryUrl,
-      this.libraryLoadCallback.bind(this),
-      this.callbackName,
-    );
-  }
+	/** loads the library, and fires this.notifyEvent upon completion */
+	loadLibrary() {
+		ScriptLoaderMap.getInstance().require(
+			this.libraryUrl,
+			this.libraryLoadCallback.bind(this),
+			this.callbackName,
+		);
+	}
 
-  connectedCallback() {
-    super.connectedCallback();
+	connectedCallback() {
+		super.connectedCallback();
 
-    this.isReady = true;
-    if (this.libraryUrl != null) this.loadLibrary();
-  }
+		this.isReady = true;
+		if (this.libraryUrl != null) this.loadLibrary();
+	}
 }
 
 @customElement("lit-google-maps-api")
 export class LitGoogleMapsApi extends JsonpLibraryElement {
-  @property({ type: String, attribute: "api-key" })
-  apiKey = "";
+	@property({ type: String, attribute: "api-key" })
+	apiKey = "";
 
-  @property({ type: String, attribute: "client-id" })
-  clientId = "";
+	@property({ type: String, attribute: "client-id" })
+	clientId = "";
 
-  @property({ type: String, attribute: "maps-url" })
-  mapsUrl = "https://maps.googleapis.com/maps/api/js?callback=%%callback%%";
+	@property({ type: String, attribute: "maps-url" })
+	mapsUrl = "https://maps.googleapis.com/maps/api/js?callback=%%callback%%";
 
-  @property({ type: String })
-  version = "3.39";
+	@property({ type: String })
+	version = "3.39";
 
-  @property({ type: String })
-  language = "";
+	@property({ type: String })
+	language = "";
 
-  @property({ type: String, attribute: "map-id" })
-  mapId = "";
+	@property({ type: String, attribute: "map-id" })
+	mapId = "";
 
-  get libraryUrl(): string {
-    return this.computeUrl(
-      this.mapsUrl,
-      this.version,
-      this.apiKey,
-      this.clientId,
-      this.language,
-      this.mapId,
-    );
-  }
+	get libraryUrl(): string {
+		return this.computeUrl(
+			this.mapsUrl,
+			this.version,
+			this.apiKey,
+			this.clientId,
+			this.language,
+			this.mapId,
+		);
+	}
 
-  get notifyEvent(): string {
-    return "api-load";
-  }
+	get notifyEvent(): string {
+		return "api-load";
+	}
 
-  computeUrl(
-    mapsUrl: string,
-    version: string,
-    apiKey: string,
-    clientId: string,
-    language: string,
-    mapId: string,
-  ): string {
-    var url = mapsUrl + "&v=" + version;
+	computeUrl(
+		mapsUrl: string,
+		version: string,
+		apiKey: string,
+		clientId: string,
+		language: string,
+		mapId: string,
+	): string {
+		var url = mapsUrl + "&v=" + version;
 
-    // Always load all Maps API libraries.
-    url += "&libraries=drawing,geometry,places,visualization,marker";
+		// Always load all Maps API libraries.
+		url += "&libraries=drawing,geometry,places,visualization,marker";
 
-    if (apiKey && !clientId) {
-      url += "&key=" + apiKey;
-    }
+		if (apiKey && !clientId) {
+			url += "&key=" + apiKey;
+		}
 
-    if (clientId) {
-      url += "&client=" + clientId;
-    }
+		if (clientId) {
+			url += "&client=" + clientId;
+		}
 
-    // Log a warning if the user is not using an API Key or Client ID.
-    if (!apiKey && !clientId) {
-      var warning =
-        "No Google Maps API Key or Client ID specified. " +
-        "See https://developers.google.com/maps/documentation/javascript/get-api-key " +
-        "for instructions to get started with a key or client id.";
-      console.warn(warning);
-    }
+		// Log a warning if the user is not using an API Key or Client ID.
+		if (!apiKey && !clientId) {
+			var warning =
+				"No Google Maps API Key or Client ID specified. " +
+				"See https://developers.google.com/maps/documentation/javascript/get-api-key " +
+				"for instructions to get started with a key or client id.";
+			console.warn(warning);
+		}
 
-    if (language) {
-      url += "&language=" + language;
-    }
+		if (language) {
+			url += "&language=" + language;
+		}
 
-    if (mapId) {
-      url += "&map_ids=" + mapId;
-    }
+		if (mapId) {
+			url += "&map_ids=" + mapId;
+		}
 
-    return url;
-  }
+		return url;
+	}
 }
